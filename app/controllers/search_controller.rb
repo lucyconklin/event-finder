0e70c558bdfad4cfae320f69506f4c5270d17f02
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-09 11:05:21 -0700

    Final Asessment
    - EventFinder service unfinished
    - See README.md for details

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
     zipcode = params[:q]
-    response = Faraday.get("http://api.eventful.com/json/events/search?location=#{zipcode}&date=March&page_size=16&sort_order=popularity&app_key=#{ENV["eventful_app_key"]}")
-    @events = JSON.parse(response.body)["events"]["event"]
+    @events = Event.for_date(params[:date])
+    # response = Faraday.get("http://api.eventful.com/json/events/search?location=#{zipcode}&date=March&page_size=16&sort_order=popularity&app_key=#{ENV["eventful_app_key"]}")
+    # @events = JSON.parse(response.body)["events"]["event"]
   end
 end
